24a5c0a9f31bad4ce58d9cacf3c587aed21a380a
Blair Brownlie <bbrownli@asu.edu>
2019-03-04 22:15:32 -0700

    adding a few bells and whistles to try get a feel for js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,8 +13,21 @@ var userGuessText = document.getElementById("userguess-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesText = document.getElementById("guesses-text");
+var mealText = document.querySelector("#meal-text");
 console.log(directionText.textContent);
-
+var today = new Date();
+var timeDay = today.getHours();
+var welcome;
+if (timeDay > 18) {
+    welcome = "The computer didn't have dinner";
+} else if (hourNow > 12) {
+    welcome = "The computer lost his lunch";
+} else if (hourNow > 0) {
+    welcome = "The computer hasn't had breakfast";
+} else {
+    welcome = "Greetings!"
+}
+mealText.textContent = welcome;
 document.onkeyup = function (event) {
     var userGuess = event.key;
     var compGuess = compChoices[Math.floor(Math.random() * compChoices.length)];
